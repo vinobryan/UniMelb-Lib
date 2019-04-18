e051d66389e8616912319ea96ce2cd88ac0493ac
zirenxiao <zirenx@student.unimelb.edu.au>
2019-04-19 04:08:51 +1000

    update project 1 2019-sm1

---
+++ Object Oriented Software Development SWEN20003_2019_SM1/oosd-project1-2019-sm1/src/World.java
@@ -0,0 +1,49 @@
+import org.newdawn.slick.Graphics;
+import org.newdawn.slick.Input;
+
+/**
+ * This class should be used to contain all the different objects in your game world, and schedule their interactions.
+ * 
+ * You are free to make ANY modifications you see fit.
+ * These classes are provided simply as a starting point. You are not strictly required to use them.
+ */
+public class World {
+	
+	private Map map;
+	private Player player;
+	private Camera camera;
+	
+	/** Construct a world, initialising map, player and camera
+	 * 
+	 */
+	public World() {
+		map = new Map();
+		player = new Player();
+		camera = new Camera(map.getMapWidth(), map.getMapHeight());
+	}
+	
+	/** Update player and camera
+	 * @param input
+	 * @param delta
+	 */
+	public void update(Input input, int delta) {
+		if (input.isMousePressed(0)) {
+			float destX = input.getMouseX() + camera.getLeft();
+			float destY = input.getMouseY() + camera.getTop();
+			player.setDestX(destX, destY);
+			player.setMove(true);
+		}
+		
+		player.update(delta, this.map);
+		camera.update(player);
+	}
+	
+	/** Render graphs
+	 * @param g
+	 */
+	public void render(Graphics g) {
+		camera.render(g);
+		map.render();
+		player.render();
+	}
+}
